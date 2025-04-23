dbc0ecb7237ce64c96cf14fde70dd087c719af57
YuanShuoHsu <ook1725@gmail.com>
2025-04-23 15:00:30 +0800

    feature: RouterBreadcrumbs add icons.

--- src/components/RouterBreadcrumbs/index.tsx
+++ src/components/RouterBreadcrumbs/index.tsx
@@ -1,17 +1,27 @@
+// https://mui.com/material-ui/react-breadcrumbs/#system-IconBreadcrumbs.tsx
 // https://mui.com/material-ui/react-breadcrumbs/#system-RouterBreadcrumbs.tsx
 
 import NextLink from "next/link";
 import { usePathname } from "next/navigation";
 
-import type { LinkProps } from "@mui/material";
-import { Breadcrumbs, Link, Typography } from "@mui/material";
+import { Home, ShoppingCart } from "@mui/icons-material";
+import type { LinkProps, SvgIconProps } from "@mui/material";
+import { Breadcrumbs, Link as MuiLink, Typography } from "@mui/material";
 
-const breadcrumbNameMap: { [key: string]: string } = {
-  "/inbox": "Inbox",
-  "/inbox/important": "Important",
-  "/trash": "Trash",
-  "/spam": "Spam",
-  "/drafts": "Drafts",
+interface BreadcrumbItem {
+  icon: React.ComponentType<SvgIconProps>;
+  label: string;
+}
+
+const breadcrumbMap: { [key: string]: BreadcrumbItem } = {
+  "/": {
+    icon: Home,
+    label: "Home",
+  },
+  "/order": {
+    icon: ShoppingCart,
+    label: "Order",
+  },
 };
 
 interface LinkRouterProps extends LinkProps {
@@ -19,30 +29,58 @@ interface LinkRouterProps extends LinkProps {
   replace?: boolean;
 }
 
-const LinkRouter = ({ to, ...props }: LinkRouterProps) => {
-  return <Link component={NextLink} href={to} {...props} />;
-};
+const LinkRouter = ({ to, ...props }: LinkRouterProps) => (
+  <MuiLink component={NextLink} href={to} {...props} />
+);
 
 const RouterBreadcrumbs = () => {
   const pathname = usePathname();
   const pathnames = pathname.split("/").filter((x) => x);
 
+  const breadcrumbRootItem = breadcrumbMap["/"];
+  const { label: rootLabel, icon: RootIcon } = breadcrumbRootItem;
+
   return (
     <Breadcrumbs aria-label="breadcrumb">
-      <LinkRouter underline="hover" color="inherit" to="/">
-        Home
+      <LinkRouter
+        underline="hover"
+        color="inherit"
+        to="/"
+        sx={{ display: "flex", alignItems: "center", gap: 0.5 }}
+      >
+        <RootIcon fontSize="inherit" />
+        {rootLabel}
       </LinkRouter>
       {pathnames.map((value, index) => {
         const last = index === pathnames.length - 1;
         const to = `/${pathnames.slice(0, index + 1).join("/")}`;
 
+        const breadcrumbItem = breadcrumbMap[to];
+        const { label, icon: Icon } = breadcrumbItem;
+
         return last ? (
-          <Typography key={to} sx={{ color: "text.primary" }}>
-            {breadcrumbNameMap[to]}
+          <Typography
+            key={to}
+            sx={{
+              color: "text.primary",
+              display: "flex",
+              alignItems: "center",
+              gap: 0.5,
+            }}
+          >
+            <Icon fontSize="inherit" />
+            {label}
           </Typography>
         ) : (
-          <LinkRouter underline="hover" color="inherit" to={to} key={to}>
-            {breadcrumbNameMap[to]}
+          <LinkRouter
+            color="inherit"
+            key={to}
+            sx={{ display: "flex", alignItems: "center", gap: 0.5 }}
+            to={to}
+            underline="hover"
+          >
+            <Icon fontSize="inherit" />
+            {label}
           </LinkRouter>
         );
       })}
