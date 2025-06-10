a7f841b19520ab5d1af1528d858c0b021a6b0ca1
YuanShuoHsu <ook1725@gmail.com>
2025-06-10 17:06:18 +0800

    feat: 在首頁新增 Swiper 元件以實現自動播放的幻燈片效果，提升使用者互動體驗。

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,3 +1,52 @@
-const Home = () => {};
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import {
+  Autoplay,
+  FreeMode,
+  Keyboard,
+  Mousewheel,
+  Navigation,
+  Pagination,
+} from "swiper/modules";
+
+const Home = () => (
+  <Swiper
+    className="w-full h-[300px] bg-amber-400"
+    autoplay={{
+      delay: 2500,
+      disableOnInteraction: false,
+    }}
+    grabCursor={true}
+    freeMode={{
+      sticky: true,
+    }}
+    keyboard={{
+      enabled: true,
+    }}
+    loop={true}
+    mousewheel={true}
+    navigation={true}
+    pagination={{ clickable: true }}
+    slidesPerView={1}
+    spaceBetween={0}
+    modules={[Autoplay, FreeMode, Keyboard, Mousewheel, Navigation, Pagination]}
+  >
+    {Array.from({ length: 9 }, (_, index) => (
+      <SwiperSlide
+        key={index}
+        className="flex justify-center items-center text-xl bg-green-200"
+      >
+        Slide {index + 1}
+      </SwiperSlide>
+    ))}
+  </Swiper>
+);
 
 export default Home;
