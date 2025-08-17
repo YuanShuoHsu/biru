90f860f5fe9e729c8ac9502b672d6e9d8c55c1f9
YuanShuoHsu <ook1725@gmail.com>
2025-08-17 23:42:31 +0800

    feat: 移除不必要的 Swiper CSS 引入，簡化首頁組件的依賴性。

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
 import Banner from "@/components/Banner";
 
 const Home = () => {
