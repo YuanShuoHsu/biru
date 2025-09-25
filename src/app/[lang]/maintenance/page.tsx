898f2fee5b956bf99819cc6b761e6860f960d1e9
YuanShuoHsu <ook1725@gmail.com>
2025-09-25 18:32:41 +0800

    feat: 更新維護頁面樣式，新增背景顏色和佈局設置，以提升用戶體驗。

--- src/app/[lang]/maintenance/page.tsx
+++ src/app/[lang]/maintenance/page.tsx
@@ -1,6 +1,9 @@
+// https://github.com/wayou/t-rex-runner
+
 "use client";
 
 import { useI18n } from "@/context/i18n";
+
 import { Box, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
@@ -9,9 +12,14 @@ const StyledBox = styled(Box)(({ theme }) => ({
   inset: 0,
   backgroundColor: theme.vars.palette.primary.main,
   display: "flex",
+  flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
   zIndex: theme.zIndex.appBar + 1,
+
+  [theme.getColorSchemeSelector("dark")]: {
+    backgroundColor: theme.vars.palette.background.paper,
+  },
 }));
 
 const Maintenance = () => {
