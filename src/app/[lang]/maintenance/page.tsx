987872623ffee836a73071468dd09dd4349e4fc2
YuanShuoHsu <ook1725@gmail.com>
2025-11-28 15:00:36 +0800

    feat: add MaintenanceLayout component and simplify Maintenance page structure for improved user experience during maintenance periods

--- src/app/[lang]/maintenance/page.tsx
+++ src/app/[lang]/maintenance/page.tsx
@@ -4,33 +4,15 @@
 
 import { useI18n } from "@/context/i18n";
 
-import { Box, Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
-
-const StyledBox = styled(Box)(({ theme }) => ({
-  position: "fixed",
-  inset: 0,
-  backgroundColor: theme.vars.palette.primary.main,
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  zIndex: theme.zIndex.appBar + 1,
-
-  [theme.getColorSchemeSelector("dark")]: {
-    backgroundColor: theme.vars.palette.background.paper,
-  },
-}));
+import { Typography } from "@mui/material";
 
 const Maintenance = () => {
   const dict = useI18n();
 
   return (
-    <StyledBox>
-      <Typography fontWeight="bold" variant="h4">
-        {dict.maintenance.title}
-      </Typography>
-    </StyledBox>
+    <Typography color="primary" fontWeight="bold" variant="h4">
+      {dict.maintenance.title}
+    </Typography>
   );
 };
 
