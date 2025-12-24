3d7d5d9dcb804545294d4b15f9c794a9f572b60a
YuanShuoHsu <ook1725@gmail.com>
2025-12-24 20:36:32 +0800

    refactor: remove redundant Box wrapper from Footer, consolidating padding into Container and removing unused background/border styles.

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -9,47 +9,38 @@ import Newsletter from "./Newsletter";
 import SocialLinks from "./SocialLinks";
 
 import {
-  Box,
-  type BoxProps,
   Container,
+  type ContainerProps,
   Divider,
   Grid,
   Stack,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-const StyledBox = styled(Box)<BoxProps>(({ theme }) => ({
+const StyledContainer = styled(Container)<ContainerProps>(({ theme }) => ({
   padding: theme.spacing(2),
-  backgroundColor: theme.vars.palette.background.default,
-  borderTop: `1px solid ${theme.vars.palette.divider}`,
-  transition: theme.transitions.create("background-color"),
-}));
-
-const StyledContainer = styled(Container)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   gap: theme.spacing(2),
 }));
 
 const Footer = () => (
-  <StyledBox component="footer">
-    <StyledContainer disableGutters maxWidth="lg">
-      <Grid container spacing={2}>
-        <Newsletter />
-        <LinkSection />
-      </Grid>
-      <Divider />
-      <Stack
-        direction={{ xs: "column", sm: "row" }}
-        justifyContent="space-between"
-        alignItems="center"
-        gap={1}
-      >
-        <Copyright />
-        <SocialLinks />
-      </Stack>
-    </StyledContainer>
-  </StyledBox>
+  <StyledContainer component="footer" disableGutters maxWidth="lg">
+    <Grid container spacing={2}>
+      <Newsletter />
+      <LinkSection />
+    </Grid>
+    <Divider />
+    <Stack
+      direction={{ xs: "column", sm: "row" }}
+      justifyContent="space-between"
+      alignItems="center"
+      gap={1}
+    >
+      <Copyright />
+      <SocialLinks />
+    </Stack>
+  </StyledContainer>
 );
 
 export default Footer;
