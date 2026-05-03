84b1823945a08808480b5891369df12be75728f3
許元碩 <ook1725@gmail.com>
2026-05-04 00:40:43 +0800

    refactor: improve readability of party size placeholder translation component

--- components/OrderModeDineInStoreSlugTableNumberSelect/index.tsx
+++ components/OrderModeDineInStoreSlugTableNumberSelect/index.tsx
@@ -45,7 +45,11 @@ const OrderModeDineInStoreSlugTableNumberSelect = ({
       value={""}
     >
       <MenuItem disabled value="">
-        <em>{tOrder("mode.dineIn.storeSlug.tableNumber.partySize.select.placeholder")}</em>
+        <em>
+          {tOrder(
+            "mode.dineIn.storeSlug.tableNumber.partySize.select.placeholder",
+          )}
+        </em>
       </MenuItem>
       {Array.from({ length: PARTY_SIZE_MAX }, (_, index) => index + 1).map(
         (count) => (
