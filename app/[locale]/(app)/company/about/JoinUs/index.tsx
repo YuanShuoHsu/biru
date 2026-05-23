980e9daf0c0862eb7ce3781038de228b5f31d62c
許元碩 <ook1725@gmail.com>
2026-05-23 15:49:42 +0800

    refactor: simplify JoinUs layout, update icon, and adjust container spacing

--- app/[locale]/(app)/company/about/JoinUs/index.tsx
+++ app/[locale]/(app)/company/about/JoinUs/index.tsx
@@ -2,8 +2,8 @@ import { useTranslations } from "next-intl";
 
 import GradientBox from "@/components/GradientBox";
 
-import { ArrowForward } from "@mui/icons-material";
-import { Button, Container, Stack, Typography } from "@mui/material";
+import { ChevronRight } from "@mui/icons-material";
+import { Button, Container, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 const StyledContainer = styled(Container)(({ theme }) => ({
@@ -11,7 +11,7 @@ const StyledContainer = styled(Container)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  gap: theme.spacing(5),
+  gap: theme.spacing(2),
 }));
 
 const JoinUs = () => {
@@ -19,38 +19,31 @@ const JoinUs = () => {
 
   return (
     <StyledContainer maxWidth="lg">
-      <Stack alignItems="center" gap={2}>
-        <Typography
-          color="primary.main"
-          component="h2"
-          fontWeight="bold"
-          variant="body2"
-        >
-          {tCompanyAboutJoinUs("label")}
-        </Typography>
-        <Typography
-          color="text.primary"
-          component="h2"
-          fontWeight="bold"
-          textAlign="center"
-          variant="h5"
-        >
-          <GradientBox component="span">
-            {tCompanyAboutJoinUs("titleLine1")}
-          </GradientBox>
-          <br />
-          {tCompanyAboutJoinUs("titleLine2")}
-        </Typography>
-        <Typography color="text.secondary" textAlign="center" variant="body1">
-          {tCompanyAboutJoinUs("description")}
-        </Typography>
-      </Stack>
-      <Button
-        disableElevation
-        endIcon={<ArrowForward />}
-        href="#team"
-        variant="contained"
+      <Typography
+        color="primary.main"
+        component="h2"
+        fontWeight="bold"
+        variant="body2"
       >
+        {tCompanyAboutJoinUs("label")}
+      </Typography>
+      <Typography
+        color="text.primary"
+        component="h2"
+        fontWeight="bold"
+        textAlign="center"
+        variant="h5"
+      >
+        <GradientBox component="span">
+          {tCompanyAboutJoinUs("titleLine1")}
+        </GradientBox>
+        <br />
+        {tCompanyAboutJoinUs("titleLine2")}
+      </Typography>
+      <Typography color="text.secondary" textAlign="center" variant="body1">
+        {tCompanyAboutJoinUs("description")}
+      </Typography>
+      <Button endIcon={<ChevronRight />} href="#" variant="contained">
         {tCompanyAboutJoinUs("button")}
       </Button>
     </StyledContainer>
