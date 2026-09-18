c2a898ca1e432e29dc73e9c7c2ae1b1b0436e4fb
許元碩 <ook1725@gmail.com>
2026-09-19 02:54:27 +0800

    style: move font variables to html and configure full height root layout

--- theme.ts
+++ theme.ts
@@ -264,9 +264,6 @@ const theme = createTheme({
   cssVariables: {
     colorSchemeSelector: "class",
   },
-  typography: {
-    fontFamily: "var(--font-geist-sans)",
-  },
 });
 
 export default theme;
